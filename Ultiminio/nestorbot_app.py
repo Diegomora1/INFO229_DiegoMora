748d3f6806eaa4c692b79fc88608ce066218bf69
Diegomora1 <diegomora940mx@gmail.com>
2020-11-02 04:15:01 -0300

    Desarrollo Ejercicio 1

--- Ultiminio/nestorbot_app.py
+++ Ultiminio/nestorbot_app.py
@@ -1,15 +1,15 @@
 from slack import WebClient
-from jamesbot import JamesBot
+from nestorbot import NestorBot
 import os
 
 # Create a slack client
 slack_web_client = WebClient(token=os.environ.get("SLACK_TOKEN"))
 
-# Get a new CoinBot
-james_bot = JamesBot("chatbot")
+# Get a new NestorBot
+nestor_bot = NestorBot("#chatbot")
 
 # Get the onboarding message payload
-message = james_bot.get_message_payload()
+message = nestor_bot.get_message_payload()
 
 # Post the onboarding message in Slack
 slack_web_client.chat_postMessage(**message)
